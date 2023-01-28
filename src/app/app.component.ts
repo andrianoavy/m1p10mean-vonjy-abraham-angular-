9afc0c717816c110294eb5233ede8795c323d480
andrianoavy <abrahamramilison+andrianoavy@gmail.com>
2023-01-28 12:09:52 +0300

    manampy role affichage eo @ nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
@@ -7,16 +7,20 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Garage | Mikara-Car';
+  role:string;
   constructor(public authService: AuthService, public router:Router,) { }
+  ngOnInit(): void {
+    this.role = this.authService.getRole()
+  }
   logout() {
     this.authService.doLogout()
   }
   isClient(): any {
-    return this.authService.getRole() === "Client"
+    return this.role  === "Client"
   }
   isAtelier(): any {
-    return this.authService.getRole() === "Atelier"
+    return this.role  === "Atelier"
   }
 }
